bc4f4777223a7fffbd89b688986f9fbc4c33e957
Steven Tinsley <steven.tinsley81@gmail.com>
2018-11-16 17:06:40 +0000

    transform_model.hs updated

--- transform_model.hs
+++ transform_model.hs
@@ -1,4 +1,7 @@
--- This is a development tool that transforms a modified form of the OBJ file format into the model import file format used by the engine.
+-- Game :: Dangerous code by Steven Tinsley.  You are free to use this software and view its source code.
+-- If you wish to redistribute it or use it as part of your own work, this is permitted as long as you acknowledge the work is by the abovementioned author.
+
+-- This is a development tool used to apply translations and rotations to 3D models, which uses a modified form of the OBJ file format for input and output.
 
 module Main where
 
@@ -13,9 +16,9 @@ import Data.Array.Unboxed
 import Control.Exception
 
 sub_i :: Int -> [a] -> Int -> a
-sub_i location x y =
-  if y >= length x then error ("\nList index error.  max index: " ++ show ((length x) - 1) ++ " index: " ++ show y ++ " location: " ++ show location)
-  else x !! y
+sub_i location ls i =
+  if i >= length ls then error ("List index too large.  location: " ++ show location ++ " index: " ++ show i ++ " max: " ++ show ((length ls) - 1))
+  else ls !! i
 
 data Model = Model {vertex :: [[Char]], tex_coord :: [[Char]], normal :: [[Char]], face :: [Int]} deriving Show
 
@@ -25,11 +28,6 @@ data Loader = Loader {tex_image :: [Char], mod_descrip :: [Char], data_layout ::
 
 blank_model = Model {vertex = [], tex_coord = [], normal = [], face = []}
 blank_loader = Loader {tex_image = [], mod_descrip = [], data_layout = []}
-
-i_list :: [a] -> Int -> a -> a
-i_list x i def =
-  if i >= length x then def
-  else (sub_i 0 x i)
 
 init_ :: [a] -> [a]
 init_ x = take ((length x) - 2) x
@@ -69,6 +67,9 @@ look_up a = array ((0, 0), (3, 628)) [((x, y), realToFrac ((a !! x) !! y)) | x <
 rotation_w :: Int -> UArray (Int, Int) Float -> Matrix Float
 rotation_w a look_up = fromList 4 4 [look_up ! (2, a), - look_up ! (1, a), 0, 0, look_up ! (1, a), look_up ! (2, a), 0, 0, 0, 0, 1, 0, 0, 0, 0, 1]
 
+translation :: Floating a => a -> a -> a -> Matrix a
+translation u v w = fromList 4 4 [1, 0, 0, u, 0, 1, 0, v, 0, 0, 1, w, 0, 0, 0, 1]
+
 transform :: Floating a => [Matrix a] -> Matrix a -> [Matrix a]
 transform [] mat = []
 transform (v:vs) mat = (multStd mat v) : transform vs mat
@@ -94,6 +95,11 @@ rotate_model model a look_up =
   if a > 553 then []
   else model {vertex = conv_to_model (transform (fst vector_form) (rotation_w a look_up)), normal = conv_to_model (transform (snd vector_form) (rotation_w a look_up))} : rotate_model model (a + 79) look_up
 
+translate_model :: Model -> Float -> Float -> Float -> Model
+translate_model model u v w =
+  let vector_form = conv_to_vector 0 model [] []
+  in model {vertex = conv_to_model (transform (fst vector_form) (translation u v w))}
+
 model_to_obj2 :: [Int] -> [Char]
 model_to_obj2 [] = "\n"
 model_to_obj2 (x0:x1:x2:xs) = " " ++ show x0 ++ "/" ++ show x1 ++ "/" ++ show x2 ++ model_to_obj2 xs
@@ -106,11 +112,8 @@ model_to_obj1 1 face_order model =
   if tex_coord model == [] then model_to_obj1 2 face_order model
   else "vt " ++ ((sub_i 16 (tex_coord model) 0)) ++ " " ++ ((sub_i 17 (tex_coord model) 1)) ++ "\n" ++ model_to_obj1 1 face_order (model {tex_coord = drop 2 (tex_coord model)})
 model_to_obj1 2 face_order model =
-  if normal model == [] then model_to_obj1 3 face_order model
+  if normal model == [] then []
   else "vn " ++ ((sub_i 18 (normal model) 0)) ++ " " ++ ((sub_i 19 (normal model) 1)) ++ " " ++ ((sub_i 20 (normal model) 2)) ++ "\n" ++ model_to_obj1 2 face_order (model {normal = drop 3 (normal model)})
-model_to_obj1 3 face_order model =
-  if face model == [] then []
-  else "f" ++ model_to_obj2 (take face_order (face model)) ++ model_to_obj1 3 face_order (model {face = drop face_order (face model)})
 
 model_to_obj0 :: [Model] -> [[Char]] -> Int -> [[Char]]
 model_to_obj0 [] _ face_order = []
@@ -130,7 +133,7 @@ gen_indices :: [[Char]] -> Int -> Int -> Int -> [Char] -> ([Char], Int)
 gen_indices [] mode n num_i acc = (acc, num_i)
 gen_indices (x:xs) mode n num_i acc =
   let order = ((length (splitOneOf " /" x)) - 1)
-      test = i_list (splitOn " " x) 0 "x"
+      test = (sub_i 28 (splitOn " " x) 0)
       f_ind0 = face_index (div order 2) 0
       f_ind1 = face_index (div order 3) 0
   in
@@ -165,10 +168,10 @@ check_attrib attrib i = attrib
 
 place_vert :: Model -> Int -> [Char] -> [Char] -> [Char] -> [Char] -> Int -> ([Char], Int)
 place_vert model mode vert_acc tex_acc norm_acc col_attrib c =
-  let face_vert = ((i_list (face model) 0 0) * 3) - 3
-      face_tex = ((i_list (face model) 1 0) * 2) - 2
-      face_norm = ((i_list (face model) 2 0) * 3) - 3
-      face_norm' = ((i_list (face model) 1 0) * 3) - 3
+  let face_vert = ((sub_i 29 (face model) 0)) * 3 - 3
+      face_tex = ((sub_i 30 (face model) 1)) * 2 - 2
+      face_norm = ((sub_i 31 (face model) 2)) * 3 - 3
+      face_norm' = ((sub_i 32 (face model) 1)) * 3 - 3
   in
   if face model == [] then (init_ (vert_acc ++ col_attrib ++ tex_acc ++ norm_acc), c)
   else if mode == 0 then place_vert (model {face = drop 2 (face model)}) mode (vert_acc ++ concat_ 1 (check_attrib (take 3 (drop face_vert (vertex model))) face_vert) ++ ", ") tex_acc (norm_acc ++ concat_ 0 (check_attrib (take 3 (drop face_norm' (normal model))) face_norm') ++ ", ") col_attrib (c + 1)
@@ -183,7 +186,7 @@ add_colour1 (x:xs) query =
 add_colour0 :: [[Char]] -> [Material] -> [Char] -> [Char]
 add_colour0 [] mat col = []
 add_colour0 (x:xs) mat col =
-  if x == "usemtl" then add_colour0 (drop 1 xs) mat (add_colour1 mat (i_list xs 0 "x"))
+  if x == "usemtl" then add_colour0 (drop 1 xs) mat (add_colour1 mat ((sub_i 33 xs 0)))
   else if x == "v" then add_colour0 (drop 3 xs) mat col
   else if x == "vt" then add_colour0 (drop 2 xs) mat col
   else if x == "vn" then add_colour0 (drop 3 xs) mat col
@@ -208,21 +211,22 @@ prep_meta_data :: [[Char]] -> Int -> [[Char]]
 prep_meta_data (x0:x1:x2:x3:x4:x5:xs) 16 = []
 prep_meta_data (x0:x1:x2:x3:x4:x5:xs) c = (show ((read x0) + c) ++ x1 ++ x2 ++ x3 ++ x4 ++ x5) : prep_meta_data (x0:x1:x2:x3:x4:x5:xs) (c + 2)
 
-proc_data :: [Char] -> [Char] -> Int -> UArray (Int, Int) Float -> [Char]
-proc_data meta_data vert_data face_order look_up =
+proc_data :: Int -> Float -> Float -> Float -> [Char] -> [Char] -> Int -> UArray (Int, Int) Float -> [Char]
+proc_data mode u v w meta_data vert_data face_order look_up =
   let meta_data' = prep_meta_data (splitOn ", " meta_data) 0
       model = get_vert (splitOneOf "\n /" (drop_double vert_data '/' 'x')) 0 0 blank_model
-      model_set = model : rotate_model model 79 look_up
-  in intercalate "\n~\n" (model_to_obj0 model_set meta_data' face_order)
-
---"\n\n" ++ show (model_set !! 0) ++ "\n\n" ++ show (model_set !! 1) ++ "\n\n" ++ show (model_set !! 2) ++ "\n\n" ++ show (model_set !! 3) ++ "\n\n" ++ show (model_set !! 4) ++ "\n\n" ++ show (model_set !! 5) ++ "\n\n" ++ show (model_set !! 6) ++ "\n\n" ++ show (model_set !! 7)
+      rotated_set = model : rotate_model model 79 look_up
+      translated_model = translate_model model u v w
+  in
+  if mode == 0 then intercalate "\n~\n" (model_to_obj0 [translated_model] meta_data' face_order)
+  else intercalate "\n~\n" (model_to_obj0 rotated_set meta_data' face_order)
 
 main = do
   args <- getArgs
-  h0 <- openFile ((sub_i 21 args 1)) ReadMode
-  h1 <- openFile ((sub_i 22 args 2)) WriteMode
+  h0 <- openFile ((sub_i 21 args 0)) ReadMode
+  h1 <- openFile ((sub_i 22 args 1)) WriteMode
   contents <- hGetContents h0
-  hPutStr h1 (proc_data ((sub_i 23 (splitOn "\n~\n" contents) 0)) ((sub_i 24 (splitOn "\n~\n" contents) 1)) (read ((sub_i 25 args 0))) (look_up [make_table 0 0, make_table 1 0, make_table 2 0, make_table 3 0]))
+  hPutStr h1 (proc_data (read ((sub_i 34 args 2))) (read ((sub_i 35 args 4))) (read ((sub_i 36 args 5))) (read ((sub_i 37 args 6))) ((sub_i 23 (splitOn "\n~\n" contents) 0)) ((sub_i 24 (splitOn "\n~\n" contents) 1)) (read ((sub_i 25 args 3))) (look_up [make_table 0 0, make_table 1 0, make_table 2 0, make_table 3 0]))
   hClose h0
   hClose h1
 
