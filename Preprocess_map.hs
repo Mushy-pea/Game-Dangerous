fe9e29599ebe337ad0edce54be81dde03b8b75b8
Steven Tinsley <steven.tinsley81@gmail.com>
2019-07-14 12:35:43 +0100

    Update Preprocess_map.hs

--- Preprocess_map.hs
+++ Preprocess_map.hs
@@ -15,6 +15,11 @@ import Decompress_map
 -- This is a simplified analogue of the Wall_grid type defined in Build_model, as less information is needed in this context.
 data Wall_grid = Wall_grid {u1 :: Bool, u2 :: Bool, v1 :: Bool, v2 :: Bool}
 
+single_fill [] (x:xs) = (x:xs)
+single_fill (x:xs) [] = (x:xs)
+single_fill (x:xs) (y:ys) = (x:xs)
+single_fill [] [] = []
+
 -- Initialise the simplified Wall_grid array from map file input.
 init_w_grid :: [[Char]] -> Array (Int, Int, Int) Wall_grid -> Int -> Int -> Int -> Int -> Int -> Array (Int, Int, Int) Wall_grid
 init_w_grid [] w_grid w u v u_limit v_limit = w_grid
@@ -60,14 +65,27 @@ load_floor0 (x0:x1:x2:x3:x4:xs) w u v u_limit v_limit =
   else if u == u_limit then ramp_position ++ load_floor0 xs w 0 (v + 1) u_limit v_limit
   else ramp_position ++ load_floor0 xs w (u + 1) v u_limit v_limit
 
-sim_flood2 :: [(Int, Int, Int)] -> (Int, Int) -> Int
-sim_flood2 [] (u, v) = 0
-sim_flood2 ((t, a, b):xs) (u, v) =
-  if (a, b) == (u, v) then t
-  else sim_flood2 xs (u, v)
+obj_grid_upd :: [(Int, Int, Int)] -> [((Int, Int, Int), Int)]
+obj_grid_upd [] = []
+obj_grid_upd ((w, u, v):xs) = ((w, u, v), 4) : obj_grid_upd xs
 
-sim_flood1 :: Array (Int, Int, Int) Int -> (Int, Int, Int) -> Int -> Int -> [(Int, Int, Int)]
-sim_flood1 obj_grid (w, u, v) u_limit v_limit =
+check_voxel1 :: [(Int, Int, Int)] -> (Int, Int) -> ([(Int, Int)], [(Int, Int)])
+check_voxel1 [] (u, v) = ([], [])
+check_voxel1 ((t, a, b):xs) (u, v) =
+  if (a, b) == (u, v) then
+    if t == 2 then ([(u, v)], [])
+    else ([], [(u, v)])
+  else check_voxel1 xs (u, v)
+
+check_voxel0 :: [(Int, Int, Int)] -> [(Int, Int, Int)] -> [(Int, Int)] -> [(Int, Int)] -> ([(Int, Int)], [(Int, Int)])
+check_voxel0 [] ramp_set up_ramp down_ramp = (up_ramp, down_ramp)
+check_voxel0 ((w, u, v):xs) ramp_set up_ramp down_ramp =
+  let check_voxel1_ = check_voxel1 ramp_set (u, v)
+  in check_voxel0 xs ramp_set (single_fill up_ramp (fst check_voxel1_)) (single_fill down_ramp (snd check_voxel1_))
+
+sim_flood1 :: Array (Int, Int, Int) Int -> [(Int, Int, Int)] -> Int -> Int -> [(Int, Int, Int)]
+sim_flood1 obj_grid [] u_limit v_limit = []
+sim_flood1 obj_grid ((w, u, v):xs) u_limit v_limit =
   let pos_u = if obj_grid ! (w, u + 1, v) > 0 then []
               else [(w, u + 1, v)]
       pos_v = if obj_grid ! (w, u, v + 1) > 0 then []
@@ -86,12 +104,15 @@ sim_flood1 obj_grid (w, u, v) u_limit v_limit =
                     else [(w, u + 1, v - 1)]
   in
   if u == u_limit || v == v_limit then error ("\nEdge of map reached at (" ++ show w ++ ", " ++ show u ++ ", " ++ show v ++ ").")
-  else pos_u ++ pos_v ++ neg_u ++ neg_v ++ pos_uv ++ pos_v_neg_u ++ neg_uv ++ pos_u_neg_v
+  else pos_u ++ pos_v ++ neg_u ++ neg_v ++ pos_uv ++ pos_v_neg_u ++ neg_uv ++ pos_u_neg_v ++ sim_flood1 obj_grid xs u_limit v_limit
 
-sim_flood0 :: Array (Int, Int, Int) Int -> [(Int, Int, Int)] -> [(Int, Int, Int)] -> [(Int, Int)] -> [(Int, Int)] -> ((Int, Int), (Int, Int))
-sim_flood0 obj_grid [] _ up_ramp down_ramp = (head up_ramp, head down_ramp)
-sim_flood0 obj_grid (x:xs) (y:ys) up_ramp down_ramp =
-  if 
+sim_flood0 :: Array (Int, Int, Int) Int -> [(Int, Int, Int)] -> [(Int, Int, Int)] -> [(Int, Int)] -> [(Int, Int)] -> Int -> Int -> ((Int, Int), (Int, Int))
+sim_flood0 obj_grid current_set ramp_set up_ramp down_ramp u_limit v_limit =
+  let sim_flood1_ = sim_flood1 obj_grid current_set u_limit v_limit
+      ramps_found = check_voxel0 sim_flood1_ ramp_set [] []
+  in
+  if sim_flood1_ == [] then (head up_ramp, head down_ramp)
+  else sim_flood0 (obj_grid // obj_grid_upd sim_flood1_) sim_flood1_ ramp_set (single_fill up_ramp (fst ramps_found)) (single_fill down_ramp (snd ramps_found)) u_limit v_limit
 
 main = do
   
