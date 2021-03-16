610498cc29c8bf85496cf313a6d4dc6769f5bca3
Steven Tinsley <steven.tinsley81@gmail.com>
2021-03-16 17:08:38 +0000

    The first pass refactoring of Decompress_map.hs is now complete.

--- src/Decompress_map.hs
+++ src/Decompress_map.hs
@@ -1,8 +1,9 @@
 -- Game :: Dangerous code by Steven Tinsley.  You are free to use this software and view its source code.
 -- If you wish to redistribute it or use it as part of your own work, this is permitted as long as you acknowledge the work is by the abovementioned author.
 
--- The .dan map file format encodes map geometry in a form intended to be space efficient and editable with minimal tools.  This module transforms (and expands) that text format into an intermediate text format
--- that is then parsed and used to initialise the Wall_grid and Floor_grid arrays.  This transformation was originally done by a tool chain program as a pre - processing step and was later moved into the engine.
+-- The .dan map file format encodes map geometry in a form intended to be space efficient and editable with minimal tools.  This module transforms (and expands)
+-- that text format into an intermediate text format that is then parsed and used to initialise the Wall_grid and Floor_grid arrays.  This transformation was
+-- originally done by a tool chain program as a pre - processing step and was later moved into the engine.
 -- I have retained the intermediate text format to avoid having to re - write some of the game state initialisation code, which appeared needless.
 
 module Decompress_map where
@@ -58,13 +59,15 @@ mapObject '1' obj u v w c = Just (((obj, 598) !! 0) {u__ = fromIntegral u, v__ =
 
 loadObject :: [[Char]] -> [Obj_place]
 loadObject [] = []
-loadObject (x0:x1:x2:x3:x4:x5:x6:xs) = Obj_place {ident_ = read x0, u__ = 0, v__ = 0, w__ = 0, rotation = procInts [x1, x2, x3], rotate_ = loadGrid1 x4, phase = 0, texture__ = read x5, num_elem = read x6, obj_flag = 0} : loadObject xs
+loadObject (x0:x1:x2:x3:x4:x5:x6:xs) = Obj_place {ident_ = read x0, u__ = 0, v__ = 0, w__ = 0, rotation = procInts [x1, x2, x3], rotate_ = loadGrid1 x4,
+                                                  phase = 0, texture__ = read x5, num_elem = read x6, obj_flag = 0} : loadObject xs
 
 maybeObject :: Obj_place -> [Char]
 maybeObject x =
   if ident_ x == 0 then "0, "
   else if obj_flag x > 7499 then throw Invalid_obj_flag
-  else "1, " ++ (show (ident_ x)) ++ ", " ++ (show (u__  x)) ++ ", " ++ (show (v__ x)) ++ ", " ++ (show (w__ x)) ++ ", " ++ (showInts (rotation x)) ++ (showBool (rotate_ x)) ++ (show (phase x)) ++ ", " ++ (show (texture__ x)) ++ ", " ++ (show (num_elem x)) ++ ", " ++ (show (obj_flag x)) ++ ", "
+  else "1, " ++ (show (ident_ x)) ++ ", " ++ (show (u__  x)) ++ ", " ++ (show (v__ x)) ++ ", " ++ (show (w__ x)) ++ ", " ++ (showInts (rotation x))
+       ++ (showBool (rotate_ x)) ++ (show (phase x)) ++ ", " ++ (show (texture__ x)) ++ ", " ++ (show (num_elem x)) ++ ", " ++ (show (obj_flag x)) ++ ", "
 
 -- See section 1 of the map file specification.
 wallSetup :: Char -> [Bool]
@@ -85,13 +88,6 @@ wallSetup 'n' = [True, True, False, True]
 wallSetup 'o' = [True, True, True, False]
 wallSetup 'p' = [True, True, True, True]
 
--- This function was used to add a layer of padding to the walls, which was visible to the player collision detection but has no visual presence.  This is to prevent the player model from intersecting with
--- the walls in third person view mode.  This feature has since been disabled in the engine as it is now implemented in the development tool Preprocess_map.hs.
-padWalls :: Wall_grid -> Int -> Int -> Int -> [Char]
-padWalls x u v w =
-  if u1 x == True || u2 x == True || v1 x == True || v2 x == True then show w ++ ", " ++ show u ++ ", " ++ show v ++ ", " ++ "4, 0, "
-  else []
-
 -- These three functions handle the transformation of the wall grid part of the map file into an intermediate text format.
 gridSetup2 :: Int -> Int -> Int -> Int -> Int -> Int -> Int -> [Int]
 gridSetup2 c0 c1 u v t u_max v_max =
@@ -105,20 +101,25 @@ gridSetup1 :: [Char] -> [Int] -> [Obj_place] -> Int -> Int -> Int -> Int -> Int 
 gridSetup1 [] _ obj u v w v_max c = []
 gridSetup1 (x0:x1:x2:x3:x4:x5:x6:xs) (y:ys) obj u v w v_max c =
   let ws = wallSetup x0
-      w_grid = Wall_grid {u1 = (ws, 599) !! 0, u2 = (ws, 600) !! 1, v1 = (ws, 601) !! 2, v2 = (ws, 602) !! 3, u1_bound = fromIntegral u, u2_bound = fromIntegral (u + 1), v1_bound = fromIntegral v, v2_bound = fromIntegral (v + 1), w_level = fromIntegral w, wall_flag = [c, c + 1, c + 2, c + 3], texture = [read [x1], read [x2], read [x3], read [x4]], obj = mapObject x5 obj ((div u 2) * 2) ((div v 2) * 2) w y}
+      w_grid = Wall_grid {u1 = (ws, 599) !! 0, u2 = (ws, 600) !! 1, v1 = (ws, 601) !! 2, v2 = (ws, 602) !! 3, u1_bound = fromIntegral u,
+                          u2_bound = fromIntegral (u + 1), v1_bound = fromIntegral v, v2_bound = fromIntegral (v + 1), w_level = fromIntegral w,
+                          wall_flag = [c, c + 1, c + 2, c + 3], texture = [read [x1], read [x2], read [x3], read [x4]],
+                          obj = mapObject x5 obj ((div u 2) * 2) ((div v 2) * 2) w y}
   in
   if v == v_max then w_grid : gridSetup1 xs ys obj (u + 1) 0 w v_max (c + 4)
   else w_grid : gridSetup1 xs ys obj u (v + 1) w v_max (c + 4)
 
-gridSetup0 :: [Wall_grid] -> Int -> Int -> Int -> Int -> Int -> Int -> Seq Char -> Seq Char -> (Seq Char, Seq Char)
-gridSetup0 (x:xs) u v w u_max v_max w_max acc0 acc1 =
-  let w_grid = (showBool (u1 x)) ++ (showBool (u2 x)) ++ (showBool (v1 x)) ++ (showBool (v2 x)) ++ (show (u1_bound x)) ++ ", " ++ (show (u2_bound x)) ++ ", " ++ (show (v1_bound x)) ++ ", " ++ (show (v2_bound x)) ++ ", " ++ (show (w_level x)) ++ ", " ++ (showInts (wall_flag x)) ++ (showInts (texture x)) ++ maybeObject (fromMaybe d_obj (obj x))
+gridSetup0 :: [Wall_grid] -> Int -> Int -> Int -> Int -> Int -> Int -> Seq Char -> Seq Char
+gridSetup0 (x:xs) u v w u_max v_max w_max acc0 =
+  let w_grid = (showBool (u1 x)) ++ (showBool (u2 x)) ++ (showBool (v1 x)) ++ (showBool (v2 x)) ++ (show (u1_bound x)) ++ ", " ++ (show (u2_bound x)) ++ ", "
+               ++ (show (v1_bound x)) ++ ", " ++ (show (v2_bound x)) ++ ", " ++ (show (w_level x)) ++ ", " ++ (showInts (wall_flag x)) ++ (showInts (texture x))
+               ++ maybeObject (fromMaybe d_obj (obj x))
   in
   if ((wall_flag x), 603) !! 3 > 119999 then throw Invalid_wall_flag
-  else if u == u_max && v == v_max && w == w_max then (acc0 >< fromList (init_ w_grid ++ "~"), acc1 >< fromList (padWalls x u v w))
-  else if u == u_max && v == v_max then gridSetup0 xs 0 0 (w + 1) u_max v_max w_max (acc0 >< fromList (init_ w_grid ++ "&")) (acc1 >< fromList (padWalls x u v w))
-  else if v == v_max then gridSetup0 xs (u + 1) 0 w u_max v_max w_max (acc0 >< fromList (init_ w_grid ++ ":")) (acc1 >< fromList (padWalls x u v w))
-  else gridSetup0 xs u (v + 1) w u_max v_max w_max (acc0 >< fromList w_grid) (acc1 >< fromList (padWalls x u v w))
+  else if u == u_max && v == v_max && w == w_max then acc0 >< fromList (init_ w_grid ++ "~")
+  else if u == u_max && v == v_max then gridSetup0 xs 0 0 (w + 1) u_max v_max w_max (acc0 >< fromList (init_ w_grid ++ "&"))
+  else if v == v_max then gridSetup0 xs (u + 1) 0 w u_max v_max w_max (acc0 >< fromList (init_ w_grid ++ ":"))
+  else gridSetup0 xs u (v + 1) w u_max v_max w_max (acc0 >< fromList w_grid)
 
 -- These two functions handle the transformation of the floor grid part of the map file into an intermediate text format.
 makeFloor1 :: Int -> Int -> Int -> Char -> [Char]
@@ -146,6 +147,8 @@ procMap pre_map u_max v_max w_max =
   let next_c = div ((u_max + 1) * (v_max + 1)) 4
       flag_seq = concat [gridSetup2 c c 0 0 0 u_max v_max | c <- [0, next_c..(next_c * w_max)]]
       c_max = 4 * (u_max + 1) * (v_max + 1) - 1
-      floor = makeFloor0 (splitOn " " (map filter0 (concat [(pre_map, 604) !! w | w <- [(w_max + 2)..(w_max + 2 + w_max)]]))) 0 0 0 ((div (u_max + 1) 2) - 1) ((div (v_max + 1) 2) - 1) w_max
-      w_grid = gridSetup0 (concat [gridSetup1 ((pre_map, 605) !! (w + 1)) (gridSetup2 (next_c * w) (next_c * w) 0 0 0 u_max v_max) (loadObject (splitOn ", " (filter1 ((pre_map, 606) !! 0)))) 0 0 w v_max ((u_max + 1) * (v_max + 1) * w * 4) | w <- [0..w_max]]) 0 0 0 u_max v_max w_max empty empty
-  in (toList (fst w_grid) ++ floor, [])
+      floor = makeFloor0 (splitOn " " (map filter0 (concat [(pre_map, 604) !! w | w <- [(w_max + 2)..(w_max + 2 + w_max)]]))) 0 0 0 ((div (u_max + 1) 2) - 1)
+                         ((div (v_max + 1) 2) - 1) w_max
+      w_grid = gridSetup0 (concat [gridSetup1 ((pre_map, 605) !! (w + 1)) (gridSetup2 (next_c * w) (next_c * w) 0 0 0 u_max v_max) (loadObject (splitOn ", " (filter1 ((pre_map, 606) !! 0)))) 0 0 w v_max ((u_max + 1) * (v_max + 1) * w * 4) | w <- [0..w_max]]) 0 0 0 u_max v_max w_max empty
+  in (toList w_grid ++ floor, [])
+
